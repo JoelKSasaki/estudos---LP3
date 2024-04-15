91b387113b7434631ae086f12d931faf1fb8e4fd
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2024-04-15 18:40:03 -0300

    Update palindromos.py

--- estudos---LP3/estudos-lp3/02-funcoes/exercícios/palindromos.py
+++ estudos---LP3/estudos-lp3/02-funcoes/exercícios/palindromos.py
@@ -1,14 +1,16 @@
 def palindromo(sentenca):
-    
+
     if(inverso(sentenca) == sentenca):
         print("palindromo")
     else:
         print("não é palindromo")
         
 def inverso(palavra):
+    palInversa = ""
     for e in palavra[::-1]:
-        print(e)
-    return palavra
+        palInversa = palInversa+ e
+    return palInversa.lower()
         
 word = input()
+word = word.lower()
 palindromo(word)
