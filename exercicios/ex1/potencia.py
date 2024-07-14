071daa09169f365e403d2197488959ae017b750a
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2024-07-14 10:31:04 -0300

    Delete exercicios directory

--- exercicios/ex1/potencia.py
+++
@@ -1,9 +0,0 @@
-from volume import volume as vol_aquario
-
-temp_desejada = input("Digite a sua temperatura desejada: ")
-temp_desejada = float(temp_desejada)
-temp_atual = input("Digite a temperatura ambiente: ")
-temp_atual = float(temp_atual)
-
-potencia = vol_aquario* 0.05 * (temp_desejada - temp_atual)
-print(f'potência necessária: {potencia}')
